133cb685dcd38861df5297bfada5589922f870ed
Harrybandral <bandralharry540@gmail.com>
2023-07-15 16:25:33 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-function MyButton() {
-  return <button>I'm a button</button>;
-}
+import React from "react"; // most important to use react
 
-export default function MyApp() {
+//fat arrow function is used in React
+
+const App = () => {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <h1>HELLO REACT</h1>
+      <p>Harry here</p>
     </div>
   );
-}
+};
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>HELLO REACT</h1>;
+//     </div>
+//   );
+// }
+
+/* How react is actually looking into it */
+
+// const App = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "HELLO REACT")
+//   );
+// };
+
+export default App;
