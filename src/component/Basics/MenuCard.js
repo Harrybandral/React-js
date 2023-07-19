75b371e6193e29fb5ee868d3279fd7afafdc4fcd
Harrybandral <bandralharry540@gmail.com>
2023-07-19 17:52:42 +0530

    commit

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -4,26 +4,32 @@ const MenuCard = ({ menuData }) => {
   //   console.log(menuData);
   return (
     <>
-      <div className="card-container">
-        <div className="card">
-          <div className="card-body">
-            <span className="card-number card-circle subtle">1</span>
-            <span className="card-author subtle">Breakfast</span>
-            <h2 className="card-title">Maggi</h2>
-            <span className="card-description subtle">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
-              provident veritatis omnis natus, eligendi nobis modi ex, a facilis
-              possimus commodi officiis exercitationem quo corrupti. Distinctio
-              esse animi voluptatibus dolor.
-            </span>
-            <div className="card-read">Read</div>
-          </div>
-          {/* <img src={image} alt="images" className="card-media" /> */}
-
-          <span className="card-tag subtle">Order Now</span>
-        </div>
-      </div>{" "}
-      */}
+      <section className="main-card--container">
+        {menuData.map((curElem) => {
+          return (
+            <>
+              <div className="card-container">
+                <div className="card">
+                  <div className="card-body">
+                    <span className="card-number card-circle subtle">1</span>
+                    <span className="card-author subtle">Breakfast</span>
+                    <h2 className="card-title">Maggi</h2>
+                    <span className="card-description subtle">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Qui, provident veritatis omnis natus, eligendi nobis modi
+                      ex, a facilis possimus commodi officiis exercitationem quo
+                      corrupti. Distinctio esse animi voluptatibus dolor.
+                    </span>
+                    <div className="card-read">Read</div>
+                  </div>
+                  {/* <img src={image} alt="images" className="card-media" /> */}
+                  <span className="card-tag subtle">Order Now</span>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </section>
     </>
   );
 };
