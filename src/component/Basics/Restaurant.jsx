3998be54ad250bea133abd78b802d54464f6de55
Harrybandral <bandralharry540@gmail.com>
2023-07-19 18:17:25 +0530

    commit

--- src/component/Basics/Restaurant.jsx
+++ src/component/Basics/Restaurant.jsx
@@ -11,6 +11,15 @@ const Restaurant = () => {
 
   return (
     <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button className="btn-group__item">Breakfast</button>
+          <button className="btn-group__item">Lunch</button>
+          <button className="btn-group__item">Afternoon</button>
+          <button className="btn-group__item">Dinner</button>
+          <button className="btn-group__item">All</button>
+        </div>
+      </nav>
       <MenuCard menuData={menuData} />
     </>
   );
